7898e824a243106b7190d8864f85deb603da83dc
KitVanDeBunt <kitlvdbunt@hotmail.com>
2014-04-07 17:17:23 +0200

    textures

--- ProjectMyth/binPNG/commonPngToAtf2.bat
+++ ProjectMyth/binPNG/commonPngToAtf2.bat
@@ -1,8 +1,12 @@
 ::png2atf.exe -i sky_spritesheet.png -o sky_spritesheet.atf
 @echo off
-SET TOOL_PATH="L:\Development\projects\as3\ATF Tools\tools\"
+SET TOOL_PATH="C:\Program Files (x86)\Adobe Gaming SDK 1.3\Utilities\ATF Tools\"
 
 
 copy /y nul common.atf
 call %TOOL_PATH%png2atf.exe -c -r -e -i common.png -o common.atf
+copy /y nul earth_spritesheet.atf
+call %TOOL_PATH%png2atf.exe -c -r -e -i earth_spritesheet.png -o earth_spritesheet.atf
+copy /y nul sky_spritesheet.atf
+call %TOOL_PATH%png2atf.exe -c -r -e -i sky_spritesheet.png -o sky_spritesheet.atf
 pause
