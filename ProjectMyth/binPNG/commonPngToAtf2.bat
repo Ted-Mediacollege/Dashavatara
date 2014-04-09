a03b819ebcf6f511bed75a03f66f11039908075a
KitVanDeBunt <kitlvdbunt@hotmail.com>
2014-04-09 16:50:16 +0200

    texture + sound update + level added

--- ProjectMyth/binPNG/commonPngToAtf2.bat
+++ ProjectMyth/binPNG/commonPngToAtf2.bat
@@ -3,8 +3,10 @@
 SET TOOL_PATH="C:\Program Files (x86)\Adobe Gaming SDK 1.3\Utilities\ATF Tools\"
 
 
-copy /y nul common.atf
-call %TOOL_PATH%png2atf.exe -c -r -e -i common.png -o common.atf
+copy /y nul tutorial.atf
+call %TOOL_PATH%png2atf.exe -c -r -e -i tutorial.png -o tutorial.atf
+copy /y nul editor.atf
+call %TOOL_PATH%png2atf.exe -c -r -e -i editor.png -o editor.atf
 ::copy /y nul earth_spritesheet.atf
 ::call %TOOL_PATH%png2atf.exe -c -r -e -i earth_spritesheet.png -o earth_spritesheet.atf
 ::copy /y nul sky_spritesheet.atf
